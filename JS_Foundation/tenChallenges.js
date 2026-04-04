3a7ec522fef601dd483e21b9373a58ffad49cbbe
Siddhant Chaurasiya <94628138+SiddhantChaurasiya@users.noreply.github.com>
2026-04-04 16:23:05 +0000

    tenChallenges

--- JS_Foundation/tenChallenges.js
+++ JS_Foundation/tenChallenges.js
@@ -8,4 +8,38 @@ let favouriteCity = cities[2];
 
 let teaTypes = ["herbal tea","white tea", "masala chai"];
 teaTypes[1] = "jasmine tea";
-console.log(teaTypes);
+// console.log(teaTypes);
+
+let teaOrders = ["chai", "iced tea", "matcha", "earl grey"];
+let lastOrder = teaOrders.pop();
+// console.log(lastOrder);
+// console.log(teaOrders);
+
+
+let popularTeas = ["greenTea", "oolong Tea", "chai"];
+let softCopyTeas = popularTeas;
+popularTeas.pop();
+
+// console.log(softCopyTeas);
+// console.log(popularTeas);
+
+let topCities = ["Berlin","Singapore", "New York"];
+let hardCopyCities = [...topCities];
+
+topCities.pop();
+// console.log(topCities);
+// console.log(hardCopyCities);
+
+let europeanCities = ["Paris", "Rome"];
+let asianCities = ["Tokyo", "Bangkok"];
+
+let worldCities = [...europeanCities, ...asianCities];
+// console.log(worldCities);
+
+let teaMenu = ["masala chai", "oolong tea", "green tea", "earl grey"];
+let menuLength = teaMenu.length;
+// console.log(menuLength);
+
+let cityBucketList = ["kyoto", "London", "Cape Town", "Vancouver"];
+let isLondonInList = cityBucketList.includes("London");
+console.log(isLondonInList);
